95fd8d14f7b7c8842c095c9d00ec6307edec57c0
Apoorv Sharma <apoorv.me.01@gmail.com>
2026-02-14 15:12:03 +0530

    Update valentine name and interaction texts

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Pakhi",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -23,9 +23,9 @@ const CONFIG = {
     questions: {
         first: {
             text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            yesBtn: "Yesssiee",                                             // Text for "Yes" button
+            noBtn: "Nopes",                                               // Text for "No" button
+            secretAnswer: "I don't like you, I loveee youuu! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
